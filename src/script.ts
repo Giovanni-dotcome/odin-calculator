b63382f210741ab518e9575cf5e598b790ff76a2
Your Name <your.email@example.com>
2024-10-21 07:40:32 +0200

    4 basic ops

--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,23 @@
 // operations:
 //  - add
-function add(): number {
-  return 4;
+function add(frtOp: number, sndOp: number): number {
+  return frtOp + sndOp;
 }
-console.log(add());
 
 //  - subtract
+function sub(frtOp: number, sndOp: number): number {
+  return frtOp - sndOp;
+}
+
 //  - multiply
+function mul(frtOp: number, sndOp: number): number {
+  return frtOp * sndOp;
+}
+
 //  - divide
+function div(frtOp: number, sndOp: number): number | null {
+  return sndOp === 0 ? null :frtOp / sndOp;
+}
+
+// testing
+console.log(div(1, 0));
