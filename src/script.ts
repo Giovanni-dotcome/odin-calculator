632e0f625d7cb4ff7287e76a974381958dde682d
Your Name <your.email@example.com>
2024-10-21 08:05:57 +0200

    calculator's html scheleton

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,32 @@
 import {add, sub, mul, div} from './math.js';
 
+var firstNumber: number;
+var secondNumber: number;
+var operationType: string;
+
+
+function operate(operationType: string, firstNumber: number, secondNumber: number) {
+  switch (operationType) {
+    case '+':
+      add(firstNumber, secondNumber);
+      break;
+
+    case '-':
+      sub(firstNumber, secondNumber);
+      break;
+
+    case '*':
+      mul(firstNumber, secondNumber);
+      break;
+
+    case '/':
+      div(firstNumber, secondNumber);
+      break;
+
+    default:
+      break;
+  }
+}
 
 // testing
-console.log(div(1, 0));
+console.log(operate('*', 1, 0));
